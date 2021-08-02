6bbb1299112bea01990384d837fc37d11b2532df
Cristian Dotta <cdotta90@gmail.com>
2021-08-02 19:36:14 -0300

    Add hooks for handling contracts interactions

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,17 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import { ChakraProvider } from '@chakra-ui/react';
+import React from 'react';
+import { useWeb3, Web3Context } from '../lib/web3-utils';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
+  const web3ContextValue = useWeb3();
+
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <Web3Context.Provider value={web3ContextValue}>
+        <Component {...pageProps} />
+      </Web3Context.Provider>
     </ChakraProvider>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,24 @@
-import React, { useState, useEffect } from 'react';
+import React, { useContext } from 'react';
 import { Select } from '@chakra-ui/react';
-import { useWeb3, getContracts } from '../lib/web3-utils';
+import { useAccounts, useTokens, Web3Context } from '../lib/web3-utils';
 
 export default function Home() {
-  const { web3, loading: web3Loading } = useWeb3();
-  const [accounts, setAccounts] = useState([]);
-  const [contracts, setContracts] = useState();
+  const { loading: web3Loading } = useContext(Web3Context);
+  const { accounts } = useAccounts();
+  const { tokens } = useTokens();
 
-  useEffect(() => {
-    if (!web3Loading) {
-      const init = async () => {
-        const contractsTemp = await getContracts(web3);
-        const accountsTemp = await web3.eth.getAccounts();
-        setContracts(contractsTemp);
-        setAccounts(accountsTemp);
-      };
-      init();
-    }
-  }, [web3, web3Loading]);
-
-  const isReady = () =>
-    !web3Loading && typeof contracts !== 'undefined' && accounts.length > 0;
+  const isReady = () => !web3Loading && accounts.length > 0;
 
   return (
     <div>
       {!isReady() && <div>loading</div>}
       <div>Header</div>
-      <Select placeholder="Select option">
-        <option value="option1">Option 1</option>
-        <option value="option2">Option 2</option>
-        <option value="option3">Option 3</option>
+      <Select placeholder="Select token">
+        {tokens.map((token) => (
+          <option key={token.tokenAddress} value={token.tokenAddress}>
+            {token.ticker}
+          </option>
+        ))}
       </Select>
       <div>Footer</div>
     </div>
